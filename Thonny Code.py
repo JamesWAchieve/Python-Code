089e2ce93043af400662b24abeb6e8347a2a0f18
JamesWAchieve <103068932+JamesWAchieve@users.noreply.github.com>
2022-04-07 14:57:22 -0400

    Add files via upload

---
+++ Thonny Code.py
@@ -0,0 +1,9 @@
+# This code is to set fruits into a xyz pattern
+fruits = ["Apple" , "Bananas" , "Oranges"]
+#set x to apple
+#set y to banana
+#set z to cherry
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
